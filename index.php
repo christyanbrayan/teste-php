dc7dacb043d17887d1546a25e8cbd88471e3d0e5
christyan brayan <christyankev@gmail.com>
2024-05-17 23:12:40 -0300

    update index.php

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
+<html>
+<head>
+  <style>
+    h1 {
+      background-color: black;
+      color: white;
+    }
+  </style>
+</head>
+<body>
 <?php
 
   echo "<h1> php </h1>";
 
 ?>
+</body>
+</html>
